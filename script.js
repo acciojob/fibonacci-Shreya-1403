1749d024c2feb637c1fd2379467925e2e044facb
Shreya-1403 <67495439+Shreya-1403@users.noreply.github.com>
2023-07-27 19:04:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function fibonacci(num) {
 	 let a = 0, b = 1, c, i;
     if(num == 1)
         return a;
-	if(num==2||num==3)
+	if(num==2)
 		return b;
-    for(i = 4; i <= num; i++)
+    for(i = 3; i <= num; i++)
     {
     c = a + b; //c==1
     a = b; //a==1
